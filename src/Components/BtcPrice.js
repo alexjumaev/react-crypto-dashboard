59553362b1ae1b7c80670117d36a05fbc9f2ac7a
Mikhail Sakhnyuk <its545@yandex.ru>
2018-06-23 01:48:18 +0700

    fix code

--- src/Components/BtcPrice.js
+++ src/Components/BtcPrice.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import ccxt from 'ccxt'
+import React from "react";
+import ccxt from "ccxt";
 
 export default class BtcPrice extends React.Component {
-
   state = {
     price: 0
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -15,18 +14,13 @@ export default class BtcPrice extends React.Component {
 
   updatePrice = async () => {
     const exchange = new ccxt.binance();
-    const ticker = await exchange.fetchTicker('BTC/USDT');
+    const ticker = await exchange.fetchTicker("BTC/USDT");
     this.setState({
       price: ticker.last.toFixed(2)
-    })
+    });
   };
 
   render() {
-
-    return (
-      <div>
-        BINANCE BTC/USDT price = ${this.state.price}
-      </div>
-    )
-    };
+    return <div>BINANCE BTC/USDT price = ${this.state.price}</div>;
+  }
 }
--- src/Components/ListExchanges.js
+++ src/Components/ListExchanges.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import ccxt from 'ccxt'
+import React from "react";
+import ccxt from "ccxt";
 
-import './ListExchanges.css'
+import "./ListExchanges.css";
 
 export default class Ticker extends React.Component {
-
   render() {
     return (
-      <div className="List" >
+      <div className="List">
         Колличество поддерживаемых бирж - {ccxt.exchanges.length}
       </div>
     );
